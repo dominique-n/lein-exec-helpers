bdc4ba7593b0c7213602ad5961170898e0928cc3
dom <dominiqn@gmail.com>
2016-09-24 07:45:29 +0200

    print-case ok

--- src/lein_exec_helpers/core.clj
+++ src/lein_exec_helpers/core.clj
@@ -3,19 +3,27 @@
 
 
 
+(defn print-case 
+  ([output] (print-case nil output))
+  ([wtr output]
+   (let [f (if wtr (partial #(fn [output] (.write % output)) wtr) println)]
+     (if (sequential? output)
+       (doseq [line output]
+         (f line))
+       (f output)))))
 
 (defmacro in2out->>
   [in out & forms]
   `(with-open [rdr# (reader ~in)]
-    (if-not (string? ~out)
-      (let [res# (->> rdr# ~out ~@forms)]
-        (if (sequential? res#)
-          (doseq [line# res#]
-            (println line#))
-          (println res#)))
-      (with-open [wtr# (writer ~out)]
-        (let [res# (->> rdr# ~@forms)]
-          (if (sequential? res#)
-            (doseq [line# res#]
-              (.write wtr# (str line# "\n")))
-            (.write wtr# (str res# "\n"))))))))
+     (if-not (string? ~out)
+       (let [res# (->> rdr# ~out ~@forms)]
+         (if (sequential? res#)
+           (doseq [line# res#]
+             (println line#))
+           (println res#)))
+       (with-open [wtr# (writer ~out)]
+         (let [res# (->> rdr# ~@forms)]
+           (if (sequential? res#)
+             (doseq [line# res#]
+               (.write wtr# (str line# "\n")))
+             (.write wtr# (str res# "\n"))))))))
--- test/lein_exec_helpers/core_test.clj
+++ test/lein_exec_helpers/core_test.clj
@@ -4,6 +4,11 @@
             [clojure.java.io :refer [reader writer]]))
 
 
+(facts "About `print-case to stdout"
+       (with-out-str (print-case "hello")) => "hello\n"
+       (with-out-str (print-case ["hello" "bro"])) => "hello\nbro\n"
+       )
+
 (facts "About `in2out->>"
        (with-out-str (doall (in2out->> "dev-resources/txt.txt" line-seq
                                        (map read-string) (map #(* 10 %)) (reduce +))))
@@ -21,14 +26,12 @@
   [(after :checks (clojure.java.io/delete-file out))]
   (let [out "dev-resources/res.txt"]
     (facts "About `in2out->>"
-           (do 
-             (with-out-str 
-               (in2out->> "dev-resources/txt.txt" out line-seq
-                          (map read-string) (map #(* 10 %)) (reduce +)))
-             (slurp out) => "60\n")
-           (do 
-             (with-out-str 
-               (in2out->> "dev-resources/txt.txt" out line-seq
-                          (map read-string) (map #(* 10 %))))
-             (slurp out) => "10\n20\n30\n")
+           (do (in2out->> "dev-resources/txt.txt" out line-seq
+                          (map read-string) (map #(* 10 %)) (reduce +))
+               (slurp out)
+               => "60\n")
+           (do (in2out->> "dev-resources/txt.txt" out line-seq
+                          (map read-string) (map #(* 10 %)))
+               (slurp out)
+               => "10\n20\n30\n")
            )))
